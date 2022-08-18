43b92e108bdbdf11289921989c48f7920e822304
Yucklys <yucklys687@outlook.com>
2022-08-18 04:02:16 -0400

    feat: reorganize files and add more customization

--- launch.sh
+++ launch.sh
@@ -9,11 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 if [ "$1" == "light" ]
 then
-	polybar -c $HOME/.config/polybar/light-config nord-top &
-	polybar -c $HOME/.config/polybar/light-config nord-down &
+	polybar -c $HOME/.config/polybar/bars/light-config nord-top &
+	polybar -c $HOME/.config/polybar/bars/light-config nord-down &
 else
-	polybar -c $HOME/.config/polybar/dark-config nord-top &
-	polybar -c $HOME/.config/polybar/dark-config nord-down &
+	polybar -c $HOME/.config/polybar/bars/dark-config nord-top &
+	polybar -c $HOME/.config/polybar/bars/dark-config nord-down &
 fi
 
 echo "Bars launched..."
