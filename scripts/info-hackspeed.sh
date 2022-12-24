21ac7a35288b32e8cebd1268f4bc136fcbe2018a
Yucklys <yucklys687@outlook.com>
2022-12-24 04:14:25 -0500

    Update Keyboard scripts, support more layout.

--- scripts/info-hackspeed.sh
+++ scripts/info-hackspeed.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC2059
 
-KEYBOARD_ID="KBDFans DZ60 Keyboard"
+KEYBOARD_ID="Keychron Keychron V1 Keyboard"
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
 METRIC=cpm
-FORMAT=" %d $METRIC"
+FORMAT="# %d $METRIC"
 
 INTERVAL=20
 
@@ -14,13 +14,14 @@ INTERVAL=20
 # yourself. $3 is the key index. Use `xinput test "AT Translated Set 2 keyboard"`
 # to see key codes in real time.  Be sure to open a pull request for your
 # layout's condition!
-LAYOUT=qwerty
+LAYOUT=dvorak
 
 case "$LAYOUT" in
 	qwerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 53) || ($3 >= 52 && $3 <= 58)'; ;;
 	azerty) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 24 && $3 <= 33) || ($3 >= 37 && $3 <= 54) || ($3 >= 52 && $3 <= 57)'; ;;
 	qwertz) CONDITION='($3 >= 10 && $3 <= 20) || ($3 >= 24 && $3 <= 34) || ($3 == 36) || ($3 >= 38 && $3 <= 48) || ($3 >= 52 && $3 <= 58)'; ;;
-        dontcare) CONDITION='1'; ;; # Just register all key presses, not only letters and numbers
+    dvorak) CONDITION='($3 >= 10 && $3 <= 19) || ($3 >= 27 && $3 <= 33) || ($3 >= 38 && $3 <= 47) || ($3 >= 53 && $3 <= 61)'; ;;
+    dontcare) CONDITION='1'; ;; # Just register all key presses, not only letters and numbers
 	*) echo "Unsupported layout \"$LAYOUT\""; exit 1; ;;
 esac
 
